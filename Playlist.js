ae3b4ebccb1832e2b32fdab43c1df5da9cb1b35c
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-07-22 16:08:17 +0900

    TEST::테스트중입니다. 이윤수

--- Playlist.js
+++ Playlist.js
@@ -5,10 +5,10 @@ var service = google.youtube('v3');
 service.playlists.list(
 	{
 		key: 'AIzaSyADYJgNuh0hvCN_07d4ZF4Snb9KficArr8', //googleAPI에서 부여받은 개인 keyz
-		channelId: 'UCid83oPnsL-4ZEo8CyQr6Rg', //플레이리스트 오리지널의 id값
+		channelId: 'UCLQ_lTDcNOj7VzNeW0kZ17w', //채널의 id값
 		part: 'snippet',
 		fields:
-			'nextPageToken, items(id, snippet(title, description, publishedAt, channelId, channelTitle, tags(), thumbnails(high(url))))',
+			'nextPageToken, pageInfo, items(id, snippet(title, description, publishedAt, channelId, channelTitle, tags(), thumbnails(high(url))))',
 		//api로 요청할 정보(영상의 고유ID, 채널ID, 재생목록 제목, 내용, , 재생목록 업로드 날짜, 채널 이름, 카테고리, 썸네일 : default는 썸네일의 기본해상도입니다. 고해상도의 이미지를 원하면 default를 high로 바꾸시면 됩니다.)
 		maxResults: 50,
 	},
@@ -25,17 +25,72 @@ service.playlists.list(
 		} else {
 			let playlistList = 0;
 			console.log('검색 결과 값 : ' + playlist.length);
+			console.log('nextPageToken : ' + response.data.nextPageToken);
+			console.log('총 검색결과 : ' + JSON.stringify(response.data.pageInfo, null, 4));
 			while (playlistList < playlist.length) {
-				console.log('재생목록 id : ' + playlist[playlistList].id);
-				console.log('생성 날짜 : ' + playlist[playlistList].snippet.publishedAt);
-				console.log('채널 id : ' + playlist[playlistList].snippet.channelId);
-				console.log('제목 : ' + playlist[playlistList].snippet.title);
-				console.log('설명 : ' + playlist[playlistList].snippet.description);
-				console.log('썸네일 : ' + playlist[playlistList].snippet.thumbnails.high.url);
-				console.log('채널 이름 : ' + playlist[playlistList].snippet.channelTitle);
+				// console.log('재생목록 id : ' + playlist[playlistList].id);
+				// console.log('생성 날짜 : ' + playlist[playlistList].snippet.publishedAt);
+				// console.log('채널 id : ' + playlist[playlistList].snippet.channelId);
+				// console.log('제목 : ' + playlist[playlistList].snippet.title);
+				// console.log('설명 : ' + playlist[playlistList].snippet.description);
+				// console.log('썸네일 : ' + playlist[playlistList].snippet.thumbnails.high.url);
+				// console.log('채널 이름 : ' + playlist[playlistList].snippet.channelTitle);
 				// console.log(playlist[countPlaylistVideoList].snippet.tags);
 				playlistList++;
+			}
+			if (playlistList == 50) {
+				MaxCount(playlistList);
 			}
 		}
 	},
 );
+
+function MaxCount(number) {
+	if (number == 50) {
+		console.log(number);
+		AngryDoyoon('CDIQAA');
+	}
+}
+function AngryDoyoon(tokenKey) {
+	service.playlists.list(
+		{
+			key: 'AIzaSyADYJgNuh0hvCN_07d4ZF4Snb9KficArr8', //googleAPI에서 부여받은 개인 keyz
+			channelId: 'UCLQ_lTDcNOj7VzNeW0kZ17w', //채널의 id값
+			part: 'snippet',
+			fields:
+				'nextPageToken, pageInfo, items(id, snippet(title, description, publishedAt, channelId, channelTitle, tags(), thumbnails(high(url))))',
+			//api로 요청할 정보(영상의 고유ID, 채널ID, 재생목록 제목, 내용, , 재생목록 업로드 날짜, 채널 이름, 카테고리, 썸네일 : default는 썸네일의 기본해상도입니다. 고해상도의 이미지를 원하면 default를 high로 바꾸시면 됩니다.)
+			maxResults: 50,
+			pageToken: tokenKey,
+		},
+		function (err, response) {
+			if (err) {
+				//에러 났을 때
+				console.log('The API returned an error : ' + err);
+				return;
+			}
+			var playlist = response.data.items;
+			if (playlist.length == 0) {
+				// 가져온거 없을 때
+				console.log('검색 결과 없음.');
+			} else {
+				let playlistList = 0;
+				console.log('검색 결과 값 : ' + playlist.length);
+				console.log('nextPageToken : ' + response.data.nextPageToken);
+				console.log('총 검색결과 : ' + JSON.stringify(response.data.pageInfo, null, 4));
+				while (playlistList < playlist.length) {
+					// console.log('재생목록 id : ' + playlist[playlistList].id);
+					// console.log('생성 날짜 : ' + playlist[playlistList].snippet.publishedAt);
+					// console.log('채널 id : ' + playlist[playlistList].snippet.channelId);
+					// console.log('제목 : ' + playlist[playlistList].snippet.title);
+					// console.log('설명 : ' + playlist[playlistList].snippet.description);
+					// console.log('썸네일 : ' + playlist[playlistList].snippet.thumbnails.high.url);
+					// console.log('채널 이름 : ' + playlist[playlistList].snippet.channelTitle);
+					// console.log(playlist[countPlaylistVideoList].snippet.tags);
+					playlistList++;
+				}
+				console.log(playlistList);
+			}
+		},
+	);
+}
--- PlaylistItemVideo.js
+++ PlaylistItemVideo.js
@@ -8,9 +8,9 @@ service.playlists.list(
 		channelId: 'UCid83oPnsL-4ZEo8CyQr6Rg', //플레이리스트 오리지널의 id값
 		part: 'snippet',
 		fields:
-			'nextPageToken, items(id, snippet(title, description, publishedAt, channelId, channelTitle, tags(), thumbnails(high(url))))',
+			'prevPageToken, nextPageToken, items(id, snippet(title, description, publishedAt, channelId, channelTitle, tags(), thumbnails(high(url))))',
 		//api로 요청할 정보(영상의 고유ID, 채널ID, 재생목록 제목, 내용, , 재생목록 업로드 날짜, 채널 이름, 카테고리, 썸네일 : default는 썸네일의 기본해상도입니다. 고해상도의 이미지를 원하면 default를 high로 바꾸시면 됩니다.)
-		maxResults: 2,
+		maxResults: 3,
 	},
 	function (err, response) {
 		if (err) {
@@ -18,7 +18,7 @@ service.playlists.list(
 			console.log('The API returned an error : ' + err);
 			return;
 		}
-		var playlist = response.data.items; //요청한 정보
+		let playlist = response.data.items; //요청한 정보
 		if (playlist.length == 0) {
 			// 가져온거 없을 때
 			console.log('검색 결과 없음.');
@@ -26,23 +26,25 @@ service.playlists.list(
 			let countPlaylist = 0;
 			console.log('검색 결과 값 : ' + playlist.length); //검색된 재생목록의 수
 			while (countPlaylist < playlist.length) {
+				let playlistSnippet = playlist[countPlaylist].snippet;
 				console.log('재생목록 id : ' + playlist[countPlaylist].id);
-				console.log('생성 날짜 : ' + playlist[countPlaylist].snippet.publishedAt);
-				console.log('채널 id : ' + playlist[countPlaylist].snippet.channelId);
-				console.log('제목 : ' + playlist[countPlaylist].snippet.title);
-				console.log('설명 : ' + playlist[countPlaylist].snippet.description);
-				console.log('썸네일 : ' + playlist[countPlaylist].snippet.thumbnails.high.url);
-				console.log('채널 이름 : ' + playlist[countPlaylist].snippet.channelTitle);
+				console.log('생성 날짜 : ' + playlistSnippet.publishedAt);
+				console.log('채널 id : ' + playlistSnippet.channelId);
+				console.log('제목 : ' + playlistSnippet.title);
+				console.log('설명 : ' + playlistSnippet.description);
+				console.log('썸네일 : ' + playlistSnippet.thumbnails.high.url);
+				console.log('채널 이름 : ' + playlistSnippet.channelTitle);
 				// console.log(playlist[countPlaylistVideoList].snippet.tags);
-				let playlistId = playlist[countPlaylist].id;
+				const playlistId = playlist[countPlaylist].id; //재생목록id
 				service.playlistItems.list(
 					{
 						playlistId: playlistId, //재생목록의 id값
 						key: 'AIzaSyADYJgNuh0hvCN_07d4ZF4Snb9KficArr8', //googleAPI에서 부여받은 개인 key
 						part: 'snippet',
-						fields: 'nextPageToken, pageInfo, items(id, snippet(channelId, resourceId(videoId)))',
+						fields:
+							'prevPageToken, nextPageToken, pageInfo, items(id, snippet(channelId, resourceId(videoId)))',
 						//api로 요청할 정보(영상의 고유ID, 채널ID, 영상 ID)
-						maxResults: 5,
+						maxResults: 6,
 					},
 					function (err, response) {
 						if (err) {
@@ -50,7 +52,10 @@ service.playlists.list(
 							console.log('The API returned an error:' + err);
 							return;
 						}
-						var video = response.data.items;
+						let video = response.data.items;
+						console.log('웹드라마 제목 : ' + playlistSnippet.title);
+						console.log('재생목록의 영상 수 : ' + video.length);
+						console.log('재생목록ID : ' + playlistId);
 						if (video.length == 0) {
 							// 가져온거 없을 때
 							console.log('검색 결과 없음.');
